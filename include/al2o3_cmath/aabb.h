72c384a9e583b97e2fda73b4a3d4a1c4018f4940
deano <me@deanoc.com>
2019-06-06 15:15:59 +0300

    AABB2F and more tests

--- include/al2o3_cmath/aabb.h
+++ include/al2o3_cmath/aabb.h
@@ -6,10 +6,79 @@
 #include "al2o3_cmath/scalar.h"
 #include "al2o3_cmath/vector.h"
 
+typedef struct Math_Aabb2F {
+	Math_Vec2F_t minExtent;
+	Math_Vec2F_t maxExtent;
+} Math_Aabb2F;
+
 typedef struct Math_Aabb3F {
 	Math_Vec3F_t minExtent;
 	Math_Vec3F_t maxExtent;
 } Math_Aabb3F;
+
+
+AL2O3_EXTERN_C inline Math_Aabb2F Math_Aabb2FFromCenterAndLength(Math_Vec2F_t center, Math_Vec2F_t length) {
+	Math_Vec2F_t halfLength = Math_ScalarMulVec2F(length, 0.5f);
+	Math_Aabb2F ret = {
+		Math_SubVec2F(center, halfLength),
+		Math_AddVec2F(center, halfLength),
+	};
+	return ret;
+}
+AL2O3_EXTERN_C inline Math_Aabb2F Math_Aabb2FFromCenterAndHalfLength(Math_Vec2F_t center, Math_Vec2F_t halfLength) {
+	Math_Aabb2F ret = {
+		Math_SubVec2F(center, halfLength),
+		Math_AddVec2F(center, halfLength),
+	};
+	return ret;
+}
+
+AL2O3_EXTERN_C inline bool Math_IsValidAabb2F(Math_Aabb2F const* aabb) {
+	Math_Vec2F_t diff = Math_SubVec2F(aabb->maxExtent, aabb->minExtent);
+	return (Math_DotVec2F(diff, diff) > 0.0f);
+}
+
+AL2O3_EXTERN_C Math_Vec2F_t Math_CenterAabb2F(Math_Aabb2F const* aabb) {
+	Math_Vec2F_t total = Math_AddVec2F(aabb->maxExtent, aabb->minExtent);
+	return Math_ScalarMulVec2F(total, 0.5f);
+}
+
+AL2O3_EXTERN_C Math_Vec2F_t Math_HalfLengthAabb2F(Math_Aabb2F const* aabb) {
+	Math_Vec2F_t diff = Math_SubVec2F(aabb->maxExtent, aabb->minExtent);
+	return Math_ScalarMulVec2F(diff, 0.5f);
+}
+AL2O3_EXTERN_C void Math_SetCenterAabb2F(Math_Aabb2F* aabb, Math_Vec2F_t center) {
+	Math_Vec2F_t const oldCenter = Math_CenterAabb2F(aabb);
+
+	aabb->minExtent = Math_AddVec2F(Math_SubVec2F(aabb->minExtent, oldCenter), center);
+	aabb->maxExtent = Math_AddVec2F(Math_SubVec2F(aabb->maxExtent, oldCenter), center);
+}
+AL2O3_EXTERN_C void Math_ExpandAabb2F(Math_Aabb2F* aabb, Math_Vec2F_t by) {
+	aabb->minExtent = Math_MinVec2F(aabb->minExtent, by);
+	aabb->maxExtent = Math_MaxVec2F(aabb->maxExtent, by);
+}
+
+AL2O3_EXTERN_C void Math_UnionAabb2F(Math_Aabb2F* aabb, Math_Aabb2F* with) {
+	aabb->minExtent = Math_MinVec2F(aabb->minExtent, with->minExtent);
+	aabb->maxExtent = Math_MaxVec2F(aabb->maxExtent, with->maxExtent);
+}
+
+AL2O3_EXTERN_C void Math_IntersectionAabb2F(Math_Aabb2F* aabb, Math_Aabb2F* with) {
+	aabb->minExtent = Math_MaxVec2F(aabb->minExtent, with->minExtent);
+	aabb->maxExtent = Math_MinVec2F(aabb->maxExtent, with->maxExtent);
+}
+
+AL2O3_EXTERN_C bool Math_DoesIntersectAabb2F(Math_Aabb2F const* aabb, Math_Aabb2F const* with) {
+	Math_Vec2F_t minI = Math_MaxVec2F(aabb->minExtent, with->minExtent);
+	Math_Vec2F_t maxI = Math_MinVec2F(aabb->maxExtent, with->maxExtent);
+
+	return((minI.v[0] < maxI.v[0]) && (minI.v[1] < maxI.v[1]));
+}
+AL2O3_EXTERN_C bool Math_DoesIntersectPointAabb2F(Math_Aabb2F const* aabb, Math_Vec2F_t const with) {
+	return(
+		(with.v[0] > aabb->minExtent.v[0] && with.v[0] < aabb->maxExtent.v[0]) &&
+		(with.v[1] > aabb->minExtent.v[1] && with.v[1] < aabb->maxExtent.v[1]));
+}
 
 AL2O3_EXTERN_C inline Math_Aabb3F Math_Aabb3FFromCenterAndLength(Math_Vec3F_t center, Math_Vec3F_t length) {
 	Math_Vec3F_t halfLength = Math_ScalarMulVec3F(length, 0.5f);
--- tests/test_aabb.cpp
+++ tests/test_aabb.cpp
@@ -1,7 +1,7 @@
 #include "al2o3_cmath/aabb.h"
 #include "al2o3_catch2/catch2.hpp"
 
-TEST_CASE("Math AABB3F (C)", "[Math AABB]") {
+TEST_CASE("Math AABB3F Center And Length (C)", "[Math AABB]") {
 	
 	Math_Aabb3F const a {
 		Math_Vec3F_t{10.0f, 20.0f, 30.0f },
@@ -28,8 +28,27 @@ TEST_CASE("Math AABB3F (C)", "[Math AABB]") {
 	REQUIRE(c.x == Approx(45.0f));
 	REQUIRE(c.y == Approx(90.0f));
 	REQUIRE(c.z == Approx(135.0f));
-	REQUIRE(hl.x == Approx(45.0f));
-	REQUIRE(hl.y == Approx(90.0f));
-	REQUIRE(hl.z == Approx(135.0f));
+	REQUIRE(hl.x == Approx(55.0f));
+	REQUIRE(hl.y == Approx(110.0f));
+	REQUIRE(hl.z == Approx(165.0f));
+
+}
+
+TEST_CASE("Math AABB3F Set Center And Length (C)", "[Math AABB]") {
+
+	Math_Aabb3F a{
+		Math_Vec3F_t{0.0f, 0.0f, 0.0f },
+		Math_Vec3F_t{100.0f, 200.0f, 300.0f},
+	};
+	
+	Math_SetCenterAabb3F(&a, Math_Vec3F_t{ 20.0f, 30.0f, 40.0f });
+	Math_Vec3F_t c = Math_CenterAabb3F(&a);
+	Math_Vec3F_t hl = Math_HalfLengthAabb3F(&a);
+	REQUIRE(c.x == Approx(20.0f));
+	REQUIRE(c.y == Approx(30.0f));
+	REQUIRE(c.z == Approx(40.0f));
+	REQUIRE(hl.x == Approx(50.0f));
+	REQUIRE(hl.y == Approx(100.0f));
+	REQUIRE(hl.z == Approx(150.0f));
 
 }
